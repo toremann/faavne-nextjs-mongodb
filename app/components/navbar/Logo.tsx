2604e83e67f31c5982516ecd42b10763420ebfa7
toremann <50668920+toremann@users.noreply.github.com>
2023-09-15 14:28:16 +0200

    Navbar

---
+++ app/components/navbar/Logo.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
+const Logo = () => {
+  const router = useRouter();
+
+  return (
+    <div
+      onClick={() => router.push("/")}
+      className="font-bold text-xl text-white cursor-pointer"
+    >
+      Faavne.no
+    </div>
+  );
+};
+
+export default Logo;
---
+++ app/components/navbar/Navbar.tsx
@@ -0,0 +1,22 @@
+import Container from "../Container";
+import Logo from "./Logo";
+
+interface NavbarProps {
+  currentUser?: null;
+}
+
+const Navbar: React.FC<NavbarProps> = ({ currentUser }) => {
+  console.log("currentUser", currentUser);
+  return (
+    <div className="fixed w-full z-10 shadow-sm">
+      <div className="py-4 border-b-[1px]">
+        <Container>
+          <div className="flex flex-row items-center justify-between gap-3 md:gap-0">
+            <Logo />
+          </div>
+        </Container>
+      </div>
+    </div>
+  );
+};
+export default Navbar;
