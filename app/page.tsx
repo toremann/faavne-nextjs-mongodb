8d4d535e566fecd7769f2e561929779a8258c2dd
toremann <50668920+toremann@users.noreply.github.com>
2023-09-15 14:15:44 +0200

    prisma setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,14 @@
-export default function Home() {
-  return <div>Test</div>;
+import getCurrentUser from "./actions/getCurrentUser";
+import Container from "./components/Container";
+
+export default async function Home() {
+  const currentUser = await getCurrentUser();
+
+  return (
+    <Container>
+      <div className="pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
+        Get all the stocks
+      </div>
+    </Container>
+  );
 }
