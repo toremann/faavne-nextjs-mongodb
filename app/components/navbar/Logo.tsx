f76eee037eb2d520d4895926e9a924c6e4844fe5
toremann <50668920+toremann@users.noreply.github.com>
2023-09-17 11:09:07 +0200

    update logo

--- app/components/navbar/Logo.tsx
+++ app/components/navbar/Logo.tsx
@@ -2,12 +2,17 @@
 
 import { useRouter } from 'next/navigation';
 
+import { GiSeaDragon } from 'react-icons/Gi';
+
 const Logo = () => {
   const router = useRouter();
 
   return (
-    <div onClick={() => router.push('/')} className="font-bold text-xl text-black cursor-pointer">
-      Faavne.no
+    <div onClick={() => router.push('/')} className="flex flex-row items-center cursor-pointer group">
+      <div className="transition-colors duration-300 group-hover:text-blue-500">
+        <GiSeaDragon size={48} className="transition-fill duration-300 group-hover:fill-blue-500" />
+      </div>
+      <div className="ml-4 font-bold text-xl transition-colors duration-300 group-hover:text-blue-500">Faavne.no</div>
     </div>
   );
 };
