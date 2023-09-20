d9484c42d4092e51f7cb4dd975441d151b01504d
toremann <50668920+toremann@users.noreply.github.com>
2023-09-20 09:47:49 +0200

    fix line breaking on small devices

--- app/components/stocks/StocksHead.tsx
+++ app/components/stocks/StocksHead.tsx
@@ -12,11 +12,11 @@ const StockHead: React.FC<StockHeadProps> = ({ title, subtitle, price, pct }) =>
     <>
       <div className="flex justify-between items-start">
         <div className="text-start">
-          <div className="text-2xl font-bold">{title}</div>
+          <div className="text:text-sm md:text-2xl font-bold">{title}</div>
           <div className="font-light text-neutral-500">{subtitle}</div>
         </div>
         <div>
-          <div className="text-2xl font-bold text-right text-blue-800">{price.toFixed(2)} NOK</div>
+          <div className="text:text-sm md:text-2xl font-bold text-right text-blue-800">{price.toFixed(2)} NOK</div>
           {pct !== null && (
             <div className={`text-light text-right ${pct > 0 ? 'text-green-500' : 'text-red-500'}`}>
               {pct > 0 ? '+' : ''}
