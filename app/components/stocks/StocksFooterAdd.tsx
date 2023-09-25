2fbbb470bb1e65933f314144a84d180bc5ae12c3
toremann <50668920+toremann@users.noreply.github.com>
2023-09-25 13:28:48 +0200

    ui fix

--- app/components/stocks/StocksFooterAdd.tsx
+++ app/components/stocks/StocksFooterAdd.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { AiOutlineHeart, AiOutlinePlusCircle } from 'react-icons/ai';
+import { AiOutlineHeart, AiFillHeart, AiOutlinePlusCircle } from 'react-icons/ai';
 
 import useFavorite from '@/app/hooks/useFavorite';
 import { SafeUser } from '@/app/types';
@@ -19,7 +19,7 @@ const StocksFooterAdd: React.FC<StockFooterProps> = ({ stockId, currentUser }) =
   return (
     <div className="flex items-center justify-center p-4 space-x-4">
       <div onClick={toggleFavorite} className="transform hover:scale-110 cursor-pointer">
-        <AiOutlineHeart size={24} className={hasFavorited ? 'text-red-700 hover:text-black' : 'text-red-700 hover:text-red-500'} />
+        {hasFavorited ? <AiFillHeart size={24} /> : <AiOutlineHeart size={24} className="text-red-700 hover:text-red-500" />}
       </div>
       <div onClick={() => {}} className="transform hover:scale-110 cursor-pointer relative">
         <AiOutlinePlusCircle size={24} className="text-blue-700 hover:text-blue-500" />
--- app/components/stocks/StocksUtilBox.tsx
+++ app/components/stocks/StocksUtilBox.tsx
@@ -1,9 +1,6 @@
 'use client';
-import useFavorite from '@/app/hooks/useFavorite';
-import HeartButton from '../HeartButton';
-import PortfolioButton from '../PortfolioButton';
+
 import scoreColor from '@/app/utils/scoreColor';
-import { AiFillHeart } from 'react-icons/ai';
 
 interface StocksUtilBoxProps {
   score: number;
@@ -13,8 +10,8 @@ interface StocksUtilBoxProps {
 const StocksUtilBox: React.FC<StocksUtilBoxProps> = ({ score, utbytte }) => {
   return (
     <div className="flex items-center justify-between m-2">
-      <div className={`${scoreColor(score)} text-white font-thin py-3 rounded-lg text-center mb-2 w-full mr-2 border-2`}>Utbytte: {utbytte} NOK</div>
-      <div className={`${scoreColor(score)} text-white font-thin py-3 rounded-lg text-center mb-2 w-full mr-2 border-2`}>Score: {score}</div>
+      <div className={`${scoreColor(score)} text-white md:font-thin py-3 rounded-lg text-center mb-2 w-full border-2`}>Utbytte: {utbytte} NOK</div>
+      <div className={`${scoreColor(score)} text-white md:font-thin py-3 rounded-lg text-center mb-2 w-full border-2`}>Score: {score}</div>
     </div>
   );
 };
