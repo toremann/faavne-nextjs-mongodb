fe67e310fcea4447ec9d1841d976ee5831df024c
toremann <50668920+toremann@users.noreply.github.com>
2023-10-01 22:14:22 +0200

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import getStocks from './actions/getStocks';
 
 import Container from './components/Container';
 import { Empty } from './components/Empty';
+import Heading from './components/Heading';
 import StocksCard from './components/stocks/StocksCard';
 import scoreColor from './utils/scoreColor';
 
@@ -16,9 +17,15 @@ export default async function Home() {
 
   return (
     <Container>
+      <div className="hidden md:block lg:block xl:block"><Heading title={'Aktive ubytter'} subtitle={'Aksjer med ubytte og datoer'}/></div>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
         {stocks.map((stock: any) => {
-          return <StocksCard key={stock.id} stock={stock} currentUser={currentUser} scoreColor={scoreColor} />;
+          return (
+            
+            
+          <StocksCard key={stock.id} stock={stock} currentUser={currentUser} scoreColor={scoreColor} />
+          
+          );
         })}
       </div>
     </Container>
