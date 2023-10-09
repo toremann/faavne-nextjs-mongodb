0ca22bda90be6bcc2c3cd646bc047c205027bcec
toremann <50668920+toremann@users.noreply.github.com>
2023-10-09 22:58:01 +0200

    actions

--- app/about/About.tsx
+++ app/about/About.tsx
@@ -3,12 +3,39 @@
 import Container from '../components/Container';
 import PageHeader from '../components/PageHeader';
 
-const About = () => {
+interface AboutProps {
+  stockCount: number
+  userCount: number
+}
+
+const About: React.FC<AboutProps> = async ({ stockCount, userCount }) => {
+  const totalUsers = userCount
+  const totalStocks = stockCount
+  const randomStat = 42;
 
   return (
     <Container>
       <PageHeader title={'Om'} subtitle={'Hva er Faavne?'} />
-      
+
+      <div className="flex justify-center mt-8 space-x-4">
+        {/* Total Users Card */}
+        <div className="bg-white p-4 rounded-md shadow-md w-full max-w-md text-center">
+          <h3 className="text-md font-semibold">Users</h3>
+          <p className="text-xl font-bold text-orange-500">{totalUsers}</p>
+        </div>
+
+        {/* Total Stocks Card */}
+        <div className="bg-white p-4 rounded-md shadow-md w-full max-w-md text-center">
+          <h3 className="text-md font-semibold">Stocks</h3>
+          <p className="text-xl font-bold text-orange-500">{totalStocks}</p>
+        </div>
+
+        {/* Random Stat Card */}
+        <div className="bg-white p-4 rounded-md shadow-md w-full max-w-md text-center">
+          <h3 className="text-md font-semibold">Random Stat</h3>
+          <p className="text-xl font-bold text-orange-500">42</p>
+        </div>
+      </div>
     </Container>
   );
 };
--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,8 +1,12 @@
+import getStockLength from '../actions/getStockLength';
+import getUserLength from '../actions/getUserLength';
 import About from './About';
 
 const AboutPage = async () => {
+  const stockCount = await getStockLength()
+  const userCount = await getUserLength()
 
-  return <About />;
+  return <About stockCount={stockCount} userCount={userCount} />;
 };
 
 export default AboutPage;
