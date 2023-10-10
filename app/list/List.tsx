8c0cd639d95d670d4ca00b32b45232c6406d8086
toremann <50668920+toremann@users.noreply.github.com>
2023-10-10 22:03:20 +0200

    add list
    list off all stocks

--- app/list/List.tsx
+++ app/list/List.tsx
@@ -7,6 +7,9 @@ import PageHeader from '../components/PageHeader';
 
 import { useRouter } from 'next/navigation';
 
+import { format } from 'date-fns';
+
+
 interface ListProps {
   stocks: Stock[];
 }
@@ -18,11 +21,15 @@ const List: React.FC<ListProps> = ({ stocks }) => {
     <Container>
       <div className="max-w-screen-lg mx-auto">
         <div className="flex flex-col gap-3">
-          <PageHeader title={'Alle utbytte aksjer'} subtitle={'All utbytte aksjer'} />
+          <PageHeader title={'Alle utbytte aksjer'} subtitle={'Alle utbytte aksjer'} />
         </div>
-        {stocks.map((stock: any) => (
-          <div key={stock.isin} className="w-full sm:w-auto hover:text-sky-700 cursor-pointer" onClick={() => router.push(`/stock/${stock.isin}`)}>
-            {stock.name}
+        {stocks.map((stock: Stock) => (
+          <div key={stock.isin} className="flex hover:text-sky-700 hover:bg-slate-100 cursor-pointer m-2 p-2 border-solid border-b-2 border-gray-300">
+            <div className="flex-1 w-64 truncate" onClick={() => router.push(`/stock/${stock.isin}`)}>
+              <div className="font-bold">{stock.symbol}</div>
+              <div>{stock.name}</div>
+            </div>
+            <div className="text-justify flex-auto">{stock.dividendDate && format(stock.dividendDate, 'dd/MM/yy')}</div>
           </div>
         ))}
       </div>
