eef7be9dd2826160254ebc63ea446b76117c84f5
toremann <50668920+toremann@users.noreply.github.com>
2023-10-11 00:06:35 +0200

    Update list

--- app/list/List.tsx
+++ app/list/List.tsx
@@ -7,8 +7,8 @@ import PageHeader from '../components/PageHeader';
 
 import { useRouter } from 'next/navigation';
 
-import { format } from 'date-fns';
-
+import { format, formatDistance, isBefore } from 'date-fns';
+import { nb } from 'date-fns/locale';
 
 interface ListProps {
   stocks: Stock[];
@@ -20,16 +20,19 @@ const List: React.FC<ListProps> = ({ stocks }) => {
   return (
     <Container>
       <div className="max-w-screen-lg mx-auto">
-        <div className="flex flex-col gap-3">
-          <PageHeader title={'Alle utbytte aksjer'} subtitle={'Alle utbytte aksjer'} />
+        <div className="flex flex-col gap-3 mb-2">
+          <PageHeader title={'Alle utbytte aksjer'} subtitle={'Liste over alle utbytte aksjer'} />
         </div>
         {stocks.map((stock: Stock) => (
-          <div key={stock.isin} className="flex hover:text-sky-700 hover:bg-slate-100 cursor-pointer m-2 p-2 border-solid border-b-2 border-gray-300">
-            <div className="flex-1 w-64 truncate" onClick={() => router.push(`/stock/${stock.isin}`)}>
-              <div className="font-bold">{stock.symbol}</div>
-              <div>{stock.name}</div>
+          <div key={stock.isin} className="flex hover:text-sky-700 hover:bg-slate-100 cursor-pointer pl-2 border-solid border-b-2 border-gray-300">
+            <div className="text-sm md:text-base flex-1 truncate" onClick={() => router.push(`/stock/${stock.isin}`)}>
+              <div className="font-bold -visible">{stock.symbol}</div>
+              <div >{stock.name}</div>
             </div>
-            <div className="text-justify flex-auto">{stock.dividendDate && format(stock.dividendDate, 'dd/MM/yy')}</div>
+            <div className="text-right flex flex-col text-sm md:text-base">
+              <div>{stock.dividendDate ? format(stock.dividendDate, 'dd/MM/yy') : 'Ingen dato'}</div>
+              <div>{stock.dividendDate && formatDistance(stock.dividendDate, new Date(), { addSuffix: true, locale: nb })}</div>
+            </div>
           </div>
         ))}
       </div>
