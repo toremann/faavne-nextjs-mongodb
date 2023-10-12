76b637743bcaa565d20a1bb620bcf51a119417ec
toremann <50668920+toremann@users.noreply.github.com>
2023-10-12 23:11:57 +0200

    update pageheader

--- app/components/PageHeader.tsx
+++ app/components/PageHeader.tsx
@@ -23,14 +23,14 @@ const PageHeader: React.FC<PageHeaderProps> = ({ title, subtitle, link }) => {
   const infoBoxActive = false;
 
   return (
-    <div className="flex flex-col md:flex-row mb-2">
+    <div className="flex flex-col sm:flex-row mb-2">
       <div className="w-full">
         <div className="text:text-sm md:text-2xl font-bold">{title}</div>
         <div className="font-light text-neutral-500">{subtitle}</div>
       </div>
       {link && link.linkBool && 
-      <div className='w-full'>
-      <div className="text-sm flex md:justify-end flex-row gap-2 cursor-pointer hover:underline underline-offset-2 underline-orange-600" onClick={() => router.push(`${link.linkUrl}`)}><PiListMagnifyingGlassBold size={20} /> {link.linkText}</div>
+      <div className='w-full sm:justify-end flex'>
+      <div className="text-sm flex  flex-row gap-2 cursor-pointer hover:underline underline-offset-2 underline-orange-600" onClick={() => router.push(`${link.linkUrl}`)}><PiListMagnifyingGlassBold size={20} /> {link.linkText}</div>
       </div>}
     </div>
   );
