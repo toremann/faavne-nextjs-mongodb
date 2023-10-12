ef2acc91dcd96b64889ee55fe8db0779342a3d6d
toremann <50668920+toremann@users.noreply.github.com>
2023-10-12 15:42:55 +0200

    update pageheader
    update pageheader to be more response when using link

--- app/components/PageHeader.tsx
+++ app/components/PageHeader.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useRouter } from 'next/navigation';
-import InfoBox from './InfoBox'; // Assuming the InfoBox component is in the same directory
+import { PiListMagnifyingGlassBold } from 'react-icons/pi'
 
 interface PageHeaderProps {
   title: string;
@@ -23,12 +23,15 @@ const PageHeader: React.FC<PageHeaderProps> = ({ title, subtitle, link }) => {
   const infoBoxActive = false;
 
   return (
-    <div className="text-start flex flex-col md:flex-row mb-2">
+    <div className="flex flex-col md:flex-row mb-2">
       <div className="w-full">
         <div className="text:text-sm md:text-2xl font-bold">{title}</div>
-        {link && link.linkBool && <div className="text-sm cursor-pointer hover:underline underline-offset-2 underline-orange" onClick={() => router.push(`${link.linkUrl}`)}>{link.linkText}</div>}
         <div className="font-light text-neutral-500">{subtitle}</div>
       </div>
+      {link && link.linkBool && 
+      <div className='w-full'>
+      <div className="text-sm flex md:justify-end flex-row gap-2 cursor-pointer hover:underline underline-offset-2 underline-orange-600" onClick={() => router.push(`${link.linkUrl}`)}><PiListMagnifyingGlassBold size={20} /> {link.linkText}</div>
+      </div>}
     </div>
   );
 };
