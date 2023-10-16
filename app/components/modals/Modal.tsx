09cb19d49a3865f3c0cce3b7081189e9b1c433e5
toremann <50668920+toremann@users.noreply.github.com>
2023-10-16 08:55:06 +0200

    dark modal

--- app/components/modals/Modal.tsx
+++ app/components/modals/Modal.tsx
@@ -60,12 +60,12 @@ const Modal: React.FC<ModalProps> = ({ isOpen, onClose, onSubmit, title, body, f
       <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none bg-neutral-800/70">
         <div className="relative w-full md:w-4/6 lg:w-3/6 xl:w-2/5 my-6 mx-auto h-full lg:h-auto md:h-auto">
           <div className={`translate duration-300 h-full ${showModal ? 'translate-y-0' : 'translate-y-full'} ${showModal ? 'opacity-100' : 'opacity-0'}`}>
-            <div className="translate h-full lg:h-auto md:h-auto border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+            <div className="translate h-full lg:h-auto md:h-auto border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white dark:bg-black outline-none dark:outline dark:outline-white focus:outline-none">
               <div className="flex items-center p-6 rounded-t justify-center relative border-b-[1px]">
-                <button type="button" onClick={handleClose} className="p-1 border-0 hover:opacity-70 transition absolute left-9">
+                <button type="button" onClick={handleClose} className="p-1 border-0 hover:opacity-70 transition absolute left-9 dark:text-white">
                   <MdClose size={18} />
                 </button>
-                <div className="text-lg font-semibold">{title}</div>
+                <div className="text-lg font-semibold dark:text-white">{title}</div>
               </div>
               {/* Body */}
               <div className="relative p-6 flex-auto">{body}</div>
