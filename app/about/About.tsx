9a2cd6ae46bd47e0e784e67060af06c33bb7693b
toremann <50668920+toremann@users.noreply.github.com>
2023-10-18 13:35:08 +0200

    update about

--- app/about/About.tsx
+++ app/about/About.tsx
@@ -1,5 +1,10 @@
 'use client';
 
+// Imports
+import { TbBrandTailwind, TbBrandNextjs, TbBrandPrisma, TbBrandMongodb, TbBrandTypescript, TbBrandVercel, TbBrandGithub, TbBrandUbuntu, TbBrandGoogle } from 'react-icons/tb';
+import { IconContext } from 'react-icons';
+
+// Components
 import Container from '../components/Container';
 import PageHeader from '../components/PageHeader';
 
@@ -18,21 +23,43 @@ const About: React.FC<AboutProps> = ({ stockCount, userCount, dividendCount }) =
     <Container>
       <PageHeader title={'Om'} subtitle={'Hva er Faavne?'} />
 
-      <div className="flex justify-center mt-8 space-x-4">
-        {/* Total Users Card */}
-        <div className="bg-white p-4 rounded-md shadow-md w-full max-w-md text-center">
+      <div className="bg-white p-4 rounded-md shadow-md w-full grid grid-cols-2 gap-4 divide-black divide-x divide-dotted">
+        {/* Left Side */}
+        <div className="col-span-2 md:col-span-1 p-4">
+          <h3 className="text-lg font-semibold mb-2">Utbytte aksjer</h3>
+          <p className="text-base text-gray-600 mb-4">Aksjer hentes hver time i børsens åpningstid.</p>
+          <p className="text-sm text-orange-500">Github repo...</p>
+        </div>
+
+        {/* Right Side */}
+        <div className="p-4 col-span-2 md:col-span-1">
+          <h3 className="text-md font-semibold">Tech stack:</h3>
+          <div className="flex items-center space-x-2">
+            <TbBrandNextjs size={40}/> 
+            <TbBrandTailwind size={40}/> 
+            <TbBrandPrisma size={40}/> 
+            <TbBrandMongodb size={40}/> 
+            <TbBrandTypescript size={40}/> 
+            <TbBrandVercel size={40}/> 
+            <TbBrandGithub size={40}/> 
+            <TbBrandUbuntu size={40}/> 
+            <TbBrandGoogle size={40}/>
+            </div>
+        </div>
+      </div>
+
+      <div className="flex flex-col sm:flex-row items-center mt-8 gap-4 justify-between">
+        <div className="bg-white p-4 rounded-md shadow-md w-full text-center">
           <h3 className="text-md font-semibold">Brukere</h3>
           <p className="text-xl font-bold text-orange-500">{totalUsers}</p>
         </div>
 
-        {/* Total Stocks Card */}
-        <div className="bg-white p-4 rounded-md shadow-md w-full max-w-md text-center">
+        <div className="bg-white p-4 rounded-md shadow-md w-full text-center">
           <h3 className="text-md font-semibold">Aksjer</h3>
           <p className="text-xl font-bold text-orange-500">{totalStocks}</p>
         </div>
 
-        {/* Random Stat Card */}
-        <div className="bg-white p-4 rounded-md shadow-md w-full max-w-md text-center">
+        <div className="bg-white p-4 rounded-md shadow-md w-full text-center">
           <h3 className="text-md font-semibold">Aktive utbytter</h3>
           <p className="text-xl font-bold text-orange-500">{totalDividendStocks}</p>
         </div>
