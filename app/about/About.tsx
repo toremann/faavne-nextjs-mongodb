8e3ddfe6f0266f428dae57ecd730630c59805067
toremann <50668920+toremann@users.noreply.github.com>
2023-10-18 14:47:24 +0200

    about darkmode

--- app/about/About.tsx
+++ app/about/About.tsx
@@ -23,15 +23,13 @@ const About: React.FC<AboutProps> = ({ stockCount, userCount, dividendCount }) =
     <Container>
       <PageHeader title={'Om'} subtitle={'Hva er Faavne?'} />
 
-      <div className="bg-white p-4 rounded-md shadow-md w-full grid grid-cols-2 gap-4 divide-black divide-x divide-dotted">
-        {/* Left Side */}
+      <div className="bg-white dark:bg-gray-800/75 dark:text-white p-4 rounded-md shadow-md w-full grid grid-cols-2 gap-4 divide-black divide-x divide-dotted">
         <div className="col-span-2 md:col-span-1 p-4">
           <h3 className="text-lg font-semibold mb-2">Utbytte aksjer</h3>
           <p className="text-base text-gray-600 mb-4">Aksjer hentes hver time i børsens åpningstid.</p>
           <p className="text-sm text-orange-500">Github repo...</p>
         </div>
 
-        {/* Right Side */}
         <div className="p-4 col-span-2 md:col-span-1">
           <h3 className="text-md font-semibold">Tech stack:</h3>
           <div className="flex items-center space-x-2">
@@ -49,17 +47,17 @@ const About: React.FC<AboutProps> = ({ stockCount, userCount, dividendCount }) =
       </div>
 
       <div className="flex flex-col sm:flex-row items-center mt-8 gap-4 justify-between">
-        <div className="bg-white p-4 rounded-md shadow-md w-full text-center">
+        <div className="bg-white dark:bg-gray-800/50 dark:text-white p-4 rounded-md shadow-md w-full text-center">
           <h3 className="text-md font-semibold">Brukere</h3>
           <p className="text-xl font-bold text-orange-500">{totalUsers}</p>
         </div>
 
-        <div className="bg-white p-4 rounded-md shadow-md w-full text-center">
+        <div className="bg-white dark:bg-gray-800/50 dark:text-white  p-4 rounded-md shadow-md w-full text-center">
           <h3 className="text-md font-semibold">Aksjer</h3>
           <p className="text-xl font-bold text-orange-500">{totalStocks}</p>
         </div>
 
-        <div className="bg-white p-4 rounded-md shadow-md w-full text-center">
+        <div className="bg-white dark:bg-gray-800/50 dark:text-white  p-4 rounded-md shadow-md w-full text-center">
           <h3 className="text-md font-semibold">Aktive utbytter</h3>
           <p className="text-xl font-bold text-orange-500">{totalDividendStocks}</p>
         </div>
